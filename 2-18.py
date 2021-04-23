5d36d1515e7d0dbe43ae2e8f48ed28a28cee35a7
KazumaAkiyama <68524567+KazumaAkiyama@users.noreply.github.com>
2021-04-23 15:18:09 +0900

    第二章

--- 2-18.py
+++ 2-18.py
@@ -1,5 +1,8 @@
+#ファイルを読み込んでリストにする
 with open("popular-names.txt", "r") as f:
     line_list = f.readlines()
+    #タブ区切りの3行目についてソート
     line_list.sort(key=lambda line: line.split('\t')[2], reverse=True)
+    #リストの各要素を表示
     for line in line_list:
         print(line)
