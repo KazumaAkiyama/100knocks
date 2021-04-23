d2f0d1dcdacc7525d7c492efeacee9f677235af5
KazumaAkiyama <68524567+KazumaAkiyama@users.noreply.github.com>
2021-04-23 15:08:18 +0900

    第二章

--- 2-17.py
+++ 2-17.py
@@ -1,7 +1,11 @@
+from os import name
+
+
+#ファイルを読み込んで集合に各行を追加していく
 with open("popular-names.txt", 'r') as f:
     s = set()
     for line in f:
         s.add(line.split('\t')[0])
-
-    for elem in s:
-        print(elem)
+#集合の各要素を表示
+for elem in s:
+    print(elem)
