da444ebc7bd27791573af0f4a24f21ce0230f240
KazumaAkiyama <68524567+KazumaAkiyama@users.noreply.github.com>
2021-04-30 14:53:05 +0900

    第三章

--- 第三章/3-21.py
+++ 第三章/3-21.py
@@ -18,3 +18,16 @@ text_UK = read_wiki_UK_text()
 category_line_list = get_category_line_list(text_UK)
 for line in category_line_list:
     print(line)
+
+"""
+akiyama@LAPTOP-8R9KUU89:~/100knocks/第三章$ python3 3-21.py
+[[Category:イギリス|*]]
+[[Category:イギリス連邦加盟国]]
+[[Category:英連邦王国|*]]
+[[Category:G8加盟国]]
+[[Category:欧州連合加盟国|元]]
+[[Category:海洋国家]]
+[[Category:現存する君主国]]
+[[Category:島国]]
+[[Category:1801年に成立した国家・領域]]
+"""
